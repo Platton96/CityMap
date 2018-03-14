1a305ede0cf1782f268bf2804a80cee81de4e8c0
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-14 17:31:20 +0300

    add list

--- MyCityMap/App.xaml.cs
+++ MyCityMap/App.xaml.cs
@@ -58,7 +58,7 @@ namespace MyCityMap
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(CitiesView), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
