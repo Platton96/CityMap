4abaf8d2201da9c10f4b3961fd2c87f630b4c322
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-15 15:46:10 +0300

    add class dataFromJason

---
+++ MyCityMap/Models/DataFromJason.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+namespace MyCityMap.Models
+{   [DataContract]
+    public class DataFromJason
+    {  [DataMember (Name ="photos")]
+       IEnumerable<City> Cities { get; set; }
+    }
+}
