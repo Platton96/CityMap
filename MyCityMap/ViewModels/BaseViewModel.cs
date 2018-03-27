d0d848decef1d536117a7a729caba54b818a5985
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-27 12:14:30 +0300

    Add ViewModels

---
+++ MyCityMap/ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+
+namespace MyCityMap.ViewModels
+{
+    public class BaseViewModel : Screen
+    {
+        protected readonly INavigationService PageNavigationService;
+
+        protected BaseViewModel(INavigationService pageNavigationService)
+        {
+            PageNavigationService = pageNavigationService;
+        }
+
+        public bool CanGoBack
+        {
+            get { return PageNavigationService.CanGoBack; }
+        }
+
+        protected void NavigateTo<T>()
+        {
+            PageNavigationService.Navigate<T>();
+        }
+
+        public void GoBack()
+        {
+            PageNavigationService.GoBack();
+        }
+    }
+}
